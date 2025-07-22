a2dd6f9087b7a00b1d623058caaa85c030acd83a
zayushiba07 <zayushiba07@gmail.com>
2025-07-22 18:05:55 +0530

    addition

---
+++ 1_arithemetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Enter the num1 :");
+    scanf("%d",&num1);
+    printf("Enter the num2 :");
+    scanf("%d",&num2);
+    printf("num1 is : %d\n",num1);
+    printf("num2 is : %d\n",num2);
+    printf("addition is : %d\n",num1+num2);
+    return 0;
+}

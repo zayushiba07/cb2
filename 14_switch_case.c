40e3f9ae77379c6b9afbee4c4f47edda9a84b075
zayushiba07 <zayushiba07@gmail.com>
2025-08-04 18:17:42 +0530

    nested if

---
+++ 14_switch_case.c
@@ -0,0 +1,92 @@
+/*
+
+10-99
+12
+
+two one
+
+12 % 10 = 1.2 = 1
+
+12 / 10 
+
+123 / 10 = 12 .3
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int no,first,last;
+    printf("Enter number between 10-99 :");
+    scanf("%d",&no);
+    first = no % 10;
+    last = no /10;
+    switch (first)
+    {
+        case 0:
+            printf("zero  ");
+            break;
+        case 1:
+            printf("one  ");
+            break;
+        case 2:
+            printf("two  ");
+            break;
+        case 3:
+            printf("three  ");
+            break;
+        case 4:
+            printf("four  ");
+            break;
+        case 5:
+            printf("five  ");
+            break;
+        case 6:
+            printf("six  ");
+            break;
+        case 7:
+            printf("seven  ");
+            break;
+        case 8:
+            printf("eight  ");
+            break;
+        case 9:
+            printf("nine  ");
+            break;
+    }
+     switch (last)
+    {
+        case 0:
+            printf("zero  ");
+            break;
+        case 1:
+            printf("one  ");
+            break;
+        case 2:
+            printf("two  ");
+            break;
+        case 3:
+            printf("three  ");
+            break;
+        case 4:
+            printf("four  ");
+            break;
+        case 5:
+            printf("five  ");
+            break;
+        case 6:
+            printf("six  ");
+            break;
+        case 7:
+            printf("seven  ");
+            break;
+        case 8:
+            printf("eight  ");
+            break;
+        case 9:
+            printf("nine  ");
+            break;
+    }
+    return 0;
+}
